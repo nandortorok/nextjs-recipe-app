5a47312470c4de6aa1e15033fb600c0ec82532dd
BaconPardner <nandortorok2002@gmail.com>
2022-10-30 16:41:57 +0100

    add seed

---
+++ prisma/migrations/20221030154003_init/migration.sql
@@ -0,0 +1,101 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "BaseUnit" AS ENUM ('MASS', 'VOLUME', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "unit" (
+    "id" SERIAL NOT NULL,
+    "base" "BaseUnit" NOT NULL,
+    "name" TEXT NOT NULL,
+    "short" TEXT NOT NULL,
+
+    CONSTRAINT "unit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ingredient" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "ingredient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "direction" (
+    "step_number" SERIAL NOT NULL,
+    "direction" TEXT NOT NULL,
+    "section_id" INTEGER NOT NULL,
+    "section_recipe_id" INTEGER NOT NULL,
+
+    CONSTRAINT "direction_pkey" PRIMARY KEY ("step_number")
+);
+
+-- CreateTable
+CREATE TABLE "section_ingredient" (
+    "section_id" INTEGER NOT NULL,
+    "section_recipe_id" INTEGER NOT NULL,
+    "ingredient_id" INTEGER NOT NULL,
+    "unit_id" INTEGER,
+    "amount" INTEGER,
+
+    CONSTRAINT "section_ingredient_pkey" PRIMARY KEY ("section_id","section_recipe_id","ingredient_id")
+);
+
+-- CreateTable
+CREATE TABLE "recipe" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "servings" INTEGER NOT NULL,
+    "prep_time" INTEGER NOT NULL,
+    "cook_time" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "section" (
+    "id" INTEGER NOT NULL DEFAULT 0,
+    "recipe_id" INTEGER NOT NULL,
+    "title" TEXT,
+
+    CONSTRAINT "section_pkey" PRIMARY KEY ("id","recipe_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "direction" ADD CONSTRAINT "direction_section_id_section_recipe_id_fkey" FOREIGN KEY ("section_id", "section_recipe_id") REFERENCES "section"("id", "recipe_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "section_ingredient" ADD CONSTRAINT "section_ingredient_section_id_section_recipe_id_fkey" FOREIGN KEY ("section_id", "section_recipe_id") REFERENCES "section"("id", "recipe_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "section_ingredient" ADD CONSTRAINT "section_ingredient_ingredient_id_fkey" FOREIGN KEY ("ingredient_id") REFERENCES "ingredient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "section_ingredient" ADD CONSTRAINT "section_ingredient_unit_id_fkey" FOREIGN KEY ("unit_id") REFERENCES "unit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "recipe" ADD CONSTRAINT "recipe_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "section" ADD CONSTRAINT "section_recipe_id_fkey" FOREIGN KEY ("recipe_id") REFERENCES "recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const egg = await prisma.ingredient.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      name: "egg",
+    },
+  });
+
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@bobmail.me" },
+    update: {},
+    create: {
+      email: "bob@bobmail.me",
+      name: "Bob",
+      password: "123",
+      role: "USER",
+      recipes: {
+        create: {
+          title: "Scrambled eggs",
+          prepTime: 16,
+          cookingTime: 4,
+          servings: 2,
+          recipeSections: {
+            create: {
+              id: 0,
+              ingredients: {
+                create: { ingredientId: 1, amount: 3 },
+              },
+              directions: {
+                create: [
+                  { stepNumber: 1, direction: "Prepare" },
+                  { stepNumber: 2, direction: "Cook" },
+                  { stepNumber: 3, direction: "Eat" },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  console.log({ egg, bob });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
