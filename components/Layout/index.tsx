bbceeecb471607850b092d5b14bde6d5e288d599
BaconPardner <58827769+BaconPardner@users.noreply.github.com>
2023-01-15 19:03:10 +0100

    fix next/font

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,20 @@
 import React, { PropsWithChildren } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  weight: ["100", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "optional",
+});
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Header />
-      {/* <main className="min-h-screen bg-slate-100 px-4 pt-8">{children}</main> */}
-      {children}
+      <div className={roboto.className}>{children}</div>
       <Footer />
     </>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,10 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { Roboto } from "@next/font/google";
 
 import "styles/globals.css";
 import Layout from "../components/Layout";
 import { UploadContext } from "lib/contexts";
 import useUpload from "hooks/useUpload";
-
-const roboto = Roboto({
-  weight: ["100", "400", "500", "700", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "optional",
-});
 
 export default function MyApp({
   Component,
@@ -21,14 +13,12 @@ export default function MyApp({
   const providerValue = useUpload();
 
   return (
-    <html className={roboto.className}>
-      <SessionProvider session={session}>
-        <UploadContext.Provider value={providerValue}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </UploadContext.Provider>
-      </SessionProvider>
-    </html>
+    <SessionProvider session={session}>
+      <UploadContext.Provider value={providerValue}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UploadContext.Provider>
+    </SessionProvider>
   );
 }
