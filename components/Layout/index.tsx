0b2dbc22979aced71980b39b0c035424c7979fbf
BaconPardner <58827769+BaconPardner@users.noreply.github.com>
2023-01-16 14:42:25 +0100

    add next/font as tailwind default

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,22 +1,15 @@
 import React, { PropsWithChildren } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import { Roboto } from "@next/font/google";
 
-const roboto = Roboto({
-  weight: ["100", "400", "500", "700", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "optional",
-});
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className={roboto.className}>
+    <>
       <Header />
       {children}
       <Footer />
-    </div>
+    </>
   );
 };
 export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,15 @@ import Layout from "../components/Layout";
 import { UploadContext } from "lib/contexts";
 import useUpload from "hooks/useUpload";
 
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  weight: ["100", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "optional",
+});
+
 export default function MyApp({
   Component,
   pageProps: { session, ...pageProps },
@@ -13,12 +22,19 @@ export default function MyApp({
   const providerValue = useUpload();
 
   return (
-    <SessionProvider session={session}>
-      <UploadContext.Provider value={providerValue}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </UploadContext.Provider>
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-roboto: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <UploadContext.Provider value={providerValue}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UploadContext.Provider>
+      </SessionProvider>
+    </>
   );
 }
