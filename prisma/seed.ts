2511c23599a6795a2a7bfd61fd4a4781bf43163a
Nándor Török <58827769+nandortorok@users.noreply.github.com>
2024-01-18 20:27:00 +0100

    feat: add init recipes seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,81 +1,45 @@
 import { PrismaClient } from "@prisma/client";
 
+import { units, unitConversions, users, recipes } from "./seedData.json";
+
 const prisma = new PrismaClient();
 
 async function main() {
-  const units = await prisma.unit.createMany({
+  await prisma.unit.createMany({
     skipDuplicates: true,
-    data: [
-      { id: 1, name: "gram", short: "g" },
-      { id: 2, name: "kilogram", short: "kg" },
-      { id: 3, name: "ounce", short: "oz" },
-      { id: 4, name: "pound", short: "lb" },
-      { id: 5, name: "liter", short: "l" },
-      { id: 6, name: "milliliter", short: "ml" },
-      { id: 7, name: "gallon", short: "gal" },
-      { id: 8, name: "quart", short: "qt" },
-      { id: 9, name: "pint", short: "pt" },
-      { id: 10, name: "cup" },
-      { id: 11, name: "tablespoon", short: "tbsp" },
-      { id: 12, name: "teaspoon", short: "tsp" },
-      { id: 13, name: "drop", short: "gt" },
-      { id: 14, name: "pinch", short: "pn" },
-    ],
+    data: units,
   });
 
-  const conversions = await prisma.conversion.createMany({
+  await prisma.conversion.createMany({
     skipDuplicates: true,
-    data: [
-      {
-        imperialUnitId: 3,
-        imperialAmount: 1,
-        metricUnitId: 1,
-        metricAmount: 28.35,
-      },
-      {
-        imperialUnitId: 4,
-        imperialAmount: 1,
-        metricUnitId: 1,
-        metricAmount: 453.6,
-      },
-      {
-        imperialUnitId: 7,
-        imperialAmount: 1,
-        metricUnitId: 5,
-        metricAmount: 3.785,
-      },
-      {
-        imperialUnitId: 8,
-        imperialAmount: 1,
-        metricUnitId: 5,
-        metricAmount: 0.95,
-      },
-      {
-        imperialUnitId: 9,
-        imperialAmount: 1,
-        metricUnitId: 6,
-        metricAmount: 480,
-      },
-      {
-        imperialUnitId: 10,
-        imperialAmount: 1,
-        metricUnitId: 6,
-        metricAmount: 240,
-      },
-      {
-        imperialUnitId: 11,
-        imperialAmount: 1,
-        metricUnitId: 6,
-        metricAmount: 15,
-      },
-      {
-        imperialUnitId: 12,
-        imperialAmount: 1,
-        metricUnitId: 6,
-        metricAmount: 5,
-      },
-    ],
+    data: unitConversions,
   });
+
+  await prisma.user.createMany({
+    data: users,
+  });
+
+  for (const recipe of recipes) {
+    await prisma.ingredient.createMany({
+      data: recipe.ingredients,
+    });
+
+    await prisma.recipe.createMany({
+      data: recipe.recipes,
+    });
+
+    await prisma.section.createMany({
+      data: recipe.sections,
+    });
+
+    await prisma.sectionIngredient.createMany({
+      data: recipe.sectionIngredients,
+    });
+
+    await prisma.direction.createMany({
+      data: recipe.directions,
+    });
+  }
 }
 
 main()
